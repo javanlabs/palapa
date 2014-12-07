d6c9aae882b435d59c87c54e60a1de350d0accd1
uyab <bayu.hendra@javan.co.id>
2014-12-08 00:15:11 +0700

    add login/logout in aside

--- resources/views/layouts/frontend/aside.blade.php
+++ resources/views/layouts/frontend/aside.blade.php
@@ -1,6 +1,15 @@
+<aside>
 <ul class="list-group">
     <a class="list-group-item {{ ($page == 'case')?'active':'' }}" href="{{ route('frontend.search') }}">Cari Kasus</a>
     <a class="list-group-item {{ ($page == 'organization')?'active':'' }}" href="{{ route('frontend.organization') }}">Struktur Organisasi</a>
     <a class="list-group-item {{ ($page == 'officer')?'active':'' }}" href="{{ route('frontend.officer') }}">Daftar Jaksa</a>
     <a class="list-group-item {{ ($page == 'profile')?'active':'' }}" href="{{ route('frontend.profile') }}">Profil Kejari Jember</a>
 </ul>
+
+@if(Auth::guest())
+<a class="btn btn-default btn-block" href="{{ route('gapura.login') }}">Login</a>
+@else
+<a class="btn btn-default btn-block" href="{{ route('gapura.home') }}">Dashboard</a>
+<a class="btn btn-default btn-block" href="{{ route('gapura.logout') }}">Logout</a>
+@endif
+</aside>
