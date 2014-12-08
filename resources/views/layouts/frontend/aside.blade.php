0f6f7acc8b9285952dc6171ec0a8f7794ff2cbf4
uyab <bayu.hendra@javan.co.id>
2014-12-08 16:57:26 +0700

    styling

--- resources/views/layouts/frontend/aside.blade.php
+++ resources/views/layouts/frontend/aside.blade.php
@@ -6,12 +6,17 @@
     <a class="list-group-item {{ ($page == 'profile')?'active':'' }}" href="{{ route('frontend.profile') }}">Profil Kejari Jember</a>
 </ul>
 
+@if(Auth::check())
+<ul class="list-group">
+    <a class="list-group-item list-group-item-success {{ ($page == 'backend-cases')?'active':'' }}" href="{{ route('backend.cases.create') }}"><i class="fa fa-plus"></i> Tambah Kasus</a>
+</ul>
+@endif
+
 <ul class="list-group">
 @if(Auth::guest())
 <a class="list-group-item" href="{{ route('gapura.login') }}">Login</a>
 @else
 <a class="list-group-item {{ ($page == 'backend-dashboard')?'active':'' }}" href="{{ route('dashboard.index') }}">Dashboard</a>
-<a class="list-group-item {{ ($page == 'backend-cases')?'active':'' }}" href="{{ route('backend.cases.create') }}"><i class="fa fa-plus"></i> Tambah Kasus</a>
 <a class="list-group-item {{ ($page == 'backend-officer')?'active':'' }}" href="{{ route('backend.officers.index') }}">Manajemen SDM</a>
 <a class="list-group-item {{ ($page == 'backend-setting')?'active':'' }}" href="{{ route('setting.index') }}">Konfigurasi</a>
 <a class="list-group-item" href="{{ route('gapura.logout') }}">Logout</a>
