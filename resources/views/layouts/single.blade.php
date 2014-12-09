4290de55c5c375721f040204a57c25e4dcb6f821
uyab <bayu.hendra@javan.co.id>
2014-12-09 14:47:13 +0700

    copywrite

--- resources/views/layouts/single.blade.php
+++ resources/views/layouts/single.blade.php
@@ -3,7 +3,7 @@
     <div class="container" style="margin-top: 50px">
         <div class="paper clearfix">
         <div class="text-right" style="padding: 20px">
-            <button class="btn btn-default" onclick="javascript:window.close();"><i class="fa fa-times"></i> Tutup</button>
+            <button class="btn btn-default" onclick="javascript:window.close();"><i class="fa fa-times"></i> Tutup Halaman Ini</button>
         </div>
 
             @yield('content')
