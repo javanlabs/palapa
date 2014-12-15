3bac211b57193f4320d83a188f870eaa43efc6a9
uyab <bayu.hendra@javan.co.id>
2014-12-16 05:34:08 +0700

    enhance case creation form

--- resources/views/backend/cases/create.blade.php
+++ resources/views/backend/cases/create.blade.php
@@ -14,7 +14,7 @@
             <fieldset>
             <h4>Penerimaan SPDP</h4>
             {{ BootForm::text('Nomor SPDP', 'spdp_number') }}
-            {{ BootForm::text('Tanggal', 'start_date')->addClass('datepicker')->data('provide', 'datepicker')->data('orientation', 'bottom auto') }}
+            {{ BootForm::text('Tanggal', 'start_date')->addClass('datepicker')->data('provide', 'datepicker')->data('orientation', 'bottom auto')->data('date-today-highlight', 'true')->value(date('Y-m-d')) }}
             {{ BootForm::text('Kasus', 'kasus') }}
             {{ BootForm::textarea('Pasal yang disangkakan', 'pasal')->rows(3) }}
             </fieldset>
