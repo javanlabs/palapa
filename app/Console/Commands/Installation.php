0d7f14e05b008c3bec0d1f6ee013c75a74500ece
uyab <bayu.hendra@javan.co.id>
2014-12-16 17:47:30 +0700

    add command app:install

---
+++ app/Console/Commands/Installation.php
@@ -0,0 +1,72 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class Installation extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'app:install';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		Artisan::call('migrate');
+		Artisan::call('migrate', ['--bench' => 'eendonesia/gapura']);
+		Artisan::call('migrate', ['--bench' => 'eendonesia/moderator']);
+		Artisan::call('migrate', ['--bench' => 'eendonesia/skrip']);
+		Artisan::call('db:seed');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [
+
+		];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return [
+
+		];
+	}
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [
 		'App\Console\Commands\InspireCommand',
+		'App\Console\Commands\Installation',
 	];
 
 	/**
