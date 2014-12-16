48a61157df99efdaac23fe78cc06690da5a7250e
uyab <bayu.hendra@javan.co.id>
2014-12-16 15:47:47 +0700

    migration: add column 'icon' to sop_phase

---
+++ database/migrations/2014_12_16_154446_add_icon_to_sop_phase.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIconToSopPhase extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('users', function($table)
+        {
+            $table->string('icon');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn('icon');
+        });
+	}
+
+}
