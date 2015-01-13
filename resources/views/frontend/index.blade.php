0fc260f6f366f373b335ab9cb8e1543321fdb328
uyab <uyab.exe@gmail.com>
2015-01-13 17:18:57 +0700

    repositioning home menu

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,7 +37,7 @@
                         </td>
                     </table>
                 </td>
-                <td colspan="2" rowspan="2" class="jadwal-sidang animated fadeIn" style="padding: 10px 40px">
+                <td colspan="2" class="jadwal-sidang animated fadeIn" style="padding: 10px 40px">
                     <h3 class="caption">Jadwal Sidang Hari Ini</h3>
                     <div class="inner">
                         @forelse($cases as $item)
@@ -49,31 +49,83 @@
                 </td>
             </tr>
             <tr>
-                <td class="item item-dark">
+                <td class="item">
                     <div class="inner">
-                        <?php $item = $menu->pull(0) ?>
-                        <a href="{{ $item['url'] }}">
-                            <small class="keymap">{{ $item['keymap'] }}</small>
-                            <h3 class="title">{{ $item['title'] }}</h3>
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">1</small>
+                            <h3 class="title">Pidum</h3>
+                            <i class="fa fa-chevron-right fa-3x icon"></i>
+                        </a>
+                    </div>
+                </td>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.officer') }}">
+                            <small class="keymap">4</small>
+                            <h3 class="title">Jaksa</h3>
+                            <i class="fa fa-chevron-right fa-3x icon"></i>
+                        </a>
+                    </div>
+                </td>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">7</small>
+                            <h3 class="title">Admin</h3>
                             <i class="fa fa-chevron-right fa-3x icon"></i>
                         </a>
                     </div>
                 </td>
             </tr>
 
-            @foreach($menu->chunk(3) as $chunk)
             <tr>
-                @foreach($chunk as $item)
-                    <td class="item">
-                        <a href="{{ $item['url'] }}">
-                            <small class="keymap">{{ $item['keymap'] }}</small>
-                            <h3 class="title">{{ $item['title'] }}</h3>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">2</small>
+                            <h3 class="title">Pidsus</h3>
                             <i class="fa fa-chevron-right fa-3x icon"></i>
                         </a>
-                    </td>
-                @endforeach
+                    </div>
+                </td>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">5</small>
+                            <h3 class="title">Pembinaan</h3>
+                            <i class="fa fa-chevron-right fa-3x icon"></i>
+                        </a>
+                    </div>
+                </td>
+                <td class="item">
+                </td>
             </tr>
-            @endforeach
+
+            <tr>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">3</small>
+                            <h3 class="title">Datun</h3>
+                            <i class="fa fa-chevron-right fa-3x icon"></i>
+                        </a>
+                    </div>
+                </td>
+                <td class="item">
+                    <div class="inner">
+                        <a href="{{ route('frontend.search') }}">
+                            <small class="keymap">6</small>
+                            <h3 class="title">Intelijen</h3>
+                            <i class="fa fa-chevron-right fa-3x icon"></i>
+                        </a>
+                    </div>
+                </td>
+                <td class="item">
+                    <div class="inner">
+                    </div>
+                </td>
+            </tr>
+
         </table>
     </div>
 @stop
