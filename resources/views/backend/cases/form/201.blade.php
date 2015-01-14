bde86468dd152c6b40eb793fe1edcd056c175cd7
Wisnu Manupraba <wisnu@ngomik.com>
2015-01-14 14:05:54 +0700

    Field jaksa dan staff di create dan edit kasus

--- resources/views/backend/cases/form/201.blade.php
+++ resources/views/backend/cases/form/201.blade.php
@@ -3,5 +3,7 @@
     {{ BootForm::text('Kasus Posisi', 'kasus') }}
     {{ BootForm::textarea('Pasal yang disangkakan', 'pasal')->rows(3) }}
     {{ BootForm::select('Penyidik', 'penyidik_id')->options($penyidikLookup) }}
+    {{ BootForm::select('Jaksa Penuntut Umum', 'jaksa_id')->options($jaksaLookup) }}
+    {{ BootForm::select('Staff Administrasi', 'staff_id')->options($staffLookup) }}
 </fieldset>
 
