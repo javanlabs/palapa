1c04d63225aa226ec0bc4ecc5aac7c65ac74365c
uyab <uyab.exe@gmail.com>
2015-01-16 14:15:09 +0700

    fix stat query: hanya tampilkan kasus yg belum dihapus

--- database/migrations/2014_12_16_130654_create_view_monthly_case_phase.php
+++ database/migrations/2014_12_16_130654_create_view_monthly_case_phase.php
@@ -17,7 +17,10 @@ class CreateViewMonthlyCasePhase extends Migration {
             CREATE VIEW `v_monthly_case_phase`
             AS SELECT
             `cases_phases_history`.`case_id` AS `case_id`,max(`cases_phases_history`.`phase_id`) AS `phase_id`,month(`cases_phases_history`.`start_date`) AS `month`,year(`cases_phases_history`.`start_date`) AS `year`
-            FROM `cases_phases_history` group by `cases_phases_history`.`case_id`,month(`cases_phases_history`.`start_date`),year(`cases_phases_history`.`start_date`)
+            FROM `cases_phases_history`
+            JOIN cases ON (case_id = cases.`id`)
+            where cases.deleted_at is null
+            group by `cases_phases_history`.`case_id`,month(`cases_phases_history`.`start_date`),year(`cases_phases_history`.`start_date`)
 		');
 	}
 
