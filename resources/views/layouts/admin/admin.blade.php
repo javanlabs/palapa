efc5d778241f7937ed53c605eb2c924b7b9aa54c
uyab <uyab.exe@gmail.com>
2015-01-20 15:40:04 +0700

    fix icon

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -16,7 +16,7 @@
 
             <aside class="list-group">
                 @if(Auth::guest())
-                    <a class="list-group-item" href="{{ route('gapura.login') }}">Login</a>
+                    <a class="list-group-item" href="{{ route('gapura.login') }}"><i class="fa fa-sign-in"></i> Login</a>
                 @else
                     <a class="list-group-item {{ (isset($page) && $page == 'backend-dashboard')?'active':'' }}" href="{{ route('dashboard.index') }}"><i class="fa fa-area-chart"></i> Statistik</a>
                     <a class="list-group-item {{ (isset($page) && $page == 'backend-officer')?'active':'' }}" href="{{ route('backend.officers.index') }}"><i class="fa fa-users"></i> Manajemen SDM</a>
