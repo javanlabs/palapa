8e1f26791ac4f42d9ce5a70307bb76e1ec7ad5ad
uyab <uyab.exe@gmail.com>
2015-01-20 19:42:15 +0700

    add template: pidsus P-2

---
+++ resources/views/template/202/p-2.blade.php
@@ -0,0 +1,137 @@
+<table>
+    <tbody>
+    <tr>
+    </tr><tr>
+        <td style="text-align: center;" width="250px">
+            <strong>KEJAKSAAN NEGERI JEMBER</strong>
+        </td>
+        <td width="300px">
+        </td>
+        <td style="text-align: right;">
+            <strong data-redactor-tag="strong">
+                P-2
+            </strong>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<p style="text-align: center;">
+    <strong><u data-redactor-tag="u">SURAT PERINTAH PENYELIDIKAN</u></strong>
+    <br><strong>NOMOR: PRINT ................................</strong>
+</p>
+
+<p style="text-align: center">KEPALA KEJAKSAAN NEGERI JEMBER</p>
+
+<table>
+    <tbody>
+    <tr>
+        <td width="100px">
+            <p>
+                Dasar
+            </p>
+        </td>
+        <td width="20px">
+            <p>
+                :
+            </p>
+        </td>
+        <td>
+            <ol>
+                <li>Undang-Undang No. 8 Tahun 1981 tentang Hukum Acara Pidana pasal 284 ayat (2).</li>
+                <li>Undang undang No. 5 Tahun 1991 tentang Kejaksaan Republik Indonesia.</li>
+                <li>Undang undang No ........ Tahun ......... tentang ...........</li>
+                <li>Peraturan Pemerintah No. 27 tahun 1983 tentang Pelaksanaan	Kitab Undang Undang Hukum Acara Pidana.</li>
+                <li>KEPPRES No. 96 Tahun 1999 tentang Susunan Organisasi dan Tata Kerja Kejaksaan Republik Indonesia.</li>
+            </ol>
+        </td>
+    </tr>
+    <tr>
+        <td><p>Pertimbangan</p></td>
+        <td><p>:</p></td>
+        <td>
+            <ol>
+                <li>
+                    Laporan/informasi tanggal .................
+                    <br/>tentang, adanya tindak pidana ...........................................
+                </li>
+                <li>Sebelum dilakukan penyidikan dipandang perlu lebih dahulu melakukan penyelidikan guna menentukan dapat atau tidaknya dilakukan penyidikan.</li>
+            </ol>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <p style="text-align: center"><strong>MEMERINTAHKAN</strong></p>
+        </td>
+    </tr>
+    <tr>
+        <td><p>Kepada</p></td>
+        <td><p>:</p></td>
+        <td>
+            <p>Jaksa Penyelidik:</p>
+            <ol>
+                <li>...................................</li>
+                <li>...................................</li>
+                <li>...................................</li>
+            </ol>
+        </td>
+    </tr>
+    <tr>
+        <td><p>Untuk</p></td>
+        <td><p>:</p></td>
+        <td>
+            <ol>
+                <li>
+                    Melaksanakan penyelidikan atas kebenaran laporan/informasi
+                    <br/>nomor ................................................
+                    <br/>tanggal ................................................
+                    <br/>tentang ................................................
+                </li>
+                <li>
+                    Agar dilaksanakan dengan penuh rasa tanggung jawab serta membuat laporan hasil pelaksanannya kepada ...............
+                    <br/>................................................
+                    <br/>................................................
+                    <br/>................................................
+                    dalam waktu ................................................ hari.
+                </li>
+            </ol>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<p>Kepada yang bersangkutan untuk dilaksanakan.</p>
+
+<table>
+    <tbody><tr>
+        <td style="width: 400px"></td>
+        <td>
+            <table>
+                <tr>
+                    <td>Dikeluarkan di</td>
+                    <td>:</td>
+                    <td>.................</td>
+                </tr>
+                <tr>
+                    <td>Pada tanggal</td>
+                    <td>:</td>
+                    <td>.................</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align: center">
+                        KEPALA KEJAKSAAN NEGERI JEMBER
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    </tbody></table>
+
+<p>TEMBUSAN</p>
+<ol>
+    <li value="3">
+        Yth ..................................
+        <br/>(sebagai laporan).
+    </li>
+    <li value="4">Arsip.</li>
+</ol>
