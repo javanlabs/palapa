0b5ffe486f3d7ff5e9e8dddd8805b3b0c4ab3fcb
uyab <uyab.exe@gmail.com>
2015-01-20 20:07:35 +0700

    add template: pidsus P-5

---
+++ resources/views/template/202/p-5.blade.php
@@ -0,0 +1,128 @@
+<table>
+    <tbody>
+    <tr>
+    </tr><tr>
+        <td style="text-align: center;" width="250px">
+            <strong>KEJAKSAAN NEGERI JEMBER</strong>
+        </td>
+        <td width="300px">
+        </td>
+        <td style="text-align: right;">
+            <strong data-redactor-tag="strong">
+                P-5
+            </strong>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<table>
+    <tr>
+        <td>Perihal</td>
+        <td>: Laporan hasil penyelidikan</td>
+        <td>............................</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>
+            KEPADA YTH.
+            <br/>............................
+            <br/>............................
+            <br/>Di .....................
+        </td>
+    </tr>
+</table>
+
+<p>Bersama ini dengan hormat kami sampaikan laporan hasil penyelidikan sebagai berikut:</p>
+
+<table>
+    <tr>
+        <td style="width: 150px">1. Dasar</td>
+        <td style="width: 20px">:</td>
+        <td>
+            Surat Perintah penyelidikan dari .......................
+            <br/>No ..................... tanggal ....................
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 150px">2. Laporan dari</td>
+        <td style="width: 20px">:</td>
+        <td>...................................</td>
+    </tr>
+    <tr>
+        <td style="width: 150px">2. Laporan dari</td>
+        <td style="width: 20px">:</td>
+        <td>...................................</td>
+    </tr>
+    <tr>
+        <td style="width: 150px">3. Kasus posisi</td>
+        <td style="width: 20px">:</td>
+        <td>
+            ...................................
+            <br/>...................................
+            <br/>...................................
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 150px">4. Hasil Penyelidikan</td>
+        <td style="width: 20px">:</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td style="width: 150px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.1 Wawancara</td>
+        <td style="width: 20px">:</td>
+        <td>...................................</td>
+    </tr>
+    <tr>
+        <td style="width: 150px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.2 Bukti lain</td>
+        <td style="width: 20px">:</td>
+        <td>...................................</td>
+    </tr>
+    <tr>
+        <td style="width: 150px">5. Analisa</td>
+        <td style="width: 20px">:</td>
+        <td>...................................</td>
+    </tr>
+    <tr>
+        <td style="width: 150px">6. Kesimpulan/pendapat</td>
+        <td style="width: 20px">:</td>
+        <td>
+            ...................................
+            <br/>...................................
+            <br/>...................................
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 150px">7. Saran</td>
+        <td style="width: 20px">:</td>
+        <td>
+            ...................................
+            <br/>...................................
+            <br/>...................................
+        </td>
+    </tr>
+</table>
+
+<p>Demikian laporan kami dan mohon petunjuk.</p>
+
+<table>
+    <tbody><tr>
+        <td style="width: 400px"></td>
+        <td style="text-align: center">
+            JAKSA PENYELIDIK
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            ........................
+        </td>
+    </tr>
+    </tbody></table>
+
+<p>TEMBUSAN</p>
+<ol>
+    <li>
+        ..................................
+    </li>
+</ol>
