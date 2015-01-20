b776570e2f70ba2065493ad569bd7d0e1963ff51
uyab <uyab.exe@gmail.com>
2015-01-20 19:57:23 +0700

    add template: pidsus P-4

---
+++ resources/views/template/202/p-4.blade.php
@@ -0,0 +1,91 @@
+<table>
+    <tbody>
+    <tr>
+    </tr><tr>
+        <td style="text-align: center;" width="250px">
+            <strong>KEJAKSAAN NEGERI JEMBER</strong>
+        </td>
+        <td width="300px">
+        </td>
+        <td style="text-align: right;">
+            <strong data-redactor-tag="strong">
+                P-4
+            </strong>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<table>
+    <tr>
+        <td>Nomor</td>
+        <td>: ..........................</td>
+        <td>............................</td>
+    </tr>
+    <tr>
+        <td>Sifat</td>
+        <td>: ..........................</td>
+        <td>KEPADA YTH.</td>
+    </tr>
+    <tr>
+        <td>Lampiran</td>
+        <td>: ..........................</td>
+        <td>............................</td>
+    </tr>
+    <tr>
+        <td>Perihal</td>
+        <td>: <strong>Permintaan Keterangan</strong></td>
+        <td>
+            ............................
+            <br/>Di .....................
+        </td>
+    </tr>
+</table>
+
+<p>Dengan ini diminta keharidan Saudara pada :</p>
+
+<table>
+    <tr>
+        <td style="width: 150px">Hari</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Tanggal</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Jam</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Tempat</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Bertemu</td>
+        <td>:</td>
+    </tr>
+</table>
+
+<p>untuk dimintai keterangannya sehubungan dengan laporan pada ........... tanggal .............. tentang ............. berdasarkan Surat Perintah Penyelidikan Nomor</p>
+<p>...............................</p>
+
+<p><br/></p>
+<p>Atas kehadiran Saudara kami sampaikan terima kasih.</p>
+
+<table>
+    <tbody><tr>
+        <td style="width: 400px"></td>
+        <td>
+            KEPALA KEJAKSAAN NEGERI JEMBER
+        </td>
+    </tr>
+    </tbody></table>
+
+<p>TEMBUSAN</p>
+<ol>
+    <li>
+        ..................................
+    </li>
+    <li>Arsip.</li>
+</ol>
