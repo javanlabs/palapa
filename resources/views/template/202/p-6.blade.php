1966dc176a38bf001b0541d0289843a75fdcd357
uyab <uyab.exe@gmail.com>
2015-01-20 20:14:28 +0700

    add template: pidsus P-6

---
+++ resources/views/template/202/p-6.blade.php
@@ -0,0 +1,58 @@
+<table>
+    <tbody>
+    <tr>
+        <td style="text-align: center;" width="250px">
+            <strong><u data-redactor-tag="u">KEJAKSAAN NEGERI JEMBER</u><br>
+                "UNTUK KEADILAN"
+            </strong>
+        </td>
+        <td width="300px">
+        </td>
+        <td style="text-align: right;">
+            P-6
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<p style="text-align: center"><strong>LAPORAN TERJADINYA TINDAK PIDANA</strong></p>
+
+<p>Pada hari ini {{ $today['day'] }} tanggal {{ $today['data'] }}</p>
+<p>Saya :</p>
+
+<table>
+    <tr>
+        <td style="width: 150px">Nama</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Pangkat</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>NIP</td>
+        <td>:</td>
+    </tr>
+    <tr>
+        <td>Jabatan</td>
+        <td>: Jaksa pada Kejaksaan</td>
+    </tr>
+</table>
+
+<p>dengan ini melaporkan kepada .............. Bahwa saya telah menerima laporan dari ................ Tim Penyidik/KOMNAS HAM (dalam perkara pelanggaran HAM berat), tentang tindak pidana ............. yang diduga dilakukan oleh .......... dengan kasus posisi sebagai berikut: ..........</p>
+
+<p>Demikian Laporan Terjadinya Tindak Pidana ini saya buat dengan sebenarnya atas kekuatan Sumpah Jabatan.</p>
+
+<table>
+    <tbody><tr>
+        <td style="width: 400px"></td>
+        <td style="text-align: center">
+            YANG MELAPORKAN
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            ........................
+        </td>
+    </tr>
+    </tbody></table>
