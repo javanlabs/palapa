4798da004b38e79daa02e87aa61302f0c87025c9
uyab <uyab.exe@gmail.com>
2015-01-22 20:11:43 +0700

    styling

--- resources/views/backend/cases/byJaksa.blade.php
+++ resources/views/backend/cases/byJaksa.blade.php
@@ -9,13 +9,12 @@
             <table class="table">
                 @forelse($cases as $item)
                     <tr>
-                        <td>
-                            <div>
-                                <span class="label label-default">{{ $item['age'] }} hari</span>
-                                <span class="label label-default">{{ $item['type_name'] }}</span>
-                                <span class="label label-default">{{ $item['status_name'] }}</span>
-                            </div>
-                            <h5><a href="{{ $item['permalink'] }}" class="btn-detail">{{ $item['name'] }}</a></h5>
+                        <td style="padding: 10px 20px 20px">
+                            <h4 class="mb-0"><a href="{{ $item['permalink'] }}" class="btn-detail">{{ $item['name'] }}</a></h4>
+                            <span class="text-muted">{{ $item['spdp_number'] }}</span>
+                            <span class="label label-default">{{ $item['age'] }} hari</span>
+                            <span class="label label-default">{{ $item['type_name'] }}</span>
+                            <span class="label label-default">{{ $item['status_name'] }}</span>
                         </td>
                     </tr>
                 @empty
