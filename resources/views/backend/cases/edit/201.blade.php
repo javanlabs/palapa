d497d76dfc45c93760d797eaacb7f0c352e88956
Wisnu Manupraba <wisnu@ngomik.com>
2015-01-23 06:01:05 +0700

    edit penyidik di kasus

--- resources/views/backend/cases/edit/201.blade.php
+++ resources/views/backend/cases/edit/201.blade.php
@@ -3,7 +3,7 @@
 {{ BootForm::text('Tempat Kejadian', 'crime_place')->value($case->crime_place) }}
 {{ BootForm::text('Waktu Kejadian', 'crime_time')->addClass('datetimepicker')->value($case->crime_time) }}
 {{ BootForm::textarea('Pasal yang disangkakan', 'pasal')->rows(3)->value($case->pasal) }}
-{{ BootForm::select('Penyidik', 'penyidik_id')->options($penyidikLookup)->select($case->penyidik_id) }}
+{{ BootForm::select('Penyidik', 'penyidik_id')->options($penyidikLookup)->select('e'.$case->penyidik_id) }}
 {{ BootForm::select('Jaksa Penuntut Umum', 'jaksa_id')->options($jaksaLookup)->select($case->jaksa_id) }}
 {{ BootForm::select('Staff Administrasi', 'staff_id')->options($staffLookup)->select($case->staff_id) }}
 
