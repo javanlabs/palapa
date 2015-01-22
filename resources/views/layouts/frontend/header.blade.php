ba00cf8c0fea003c3f0d53e874e25b10bd3c4b74
uyab <uyab.exe@gmail.com>
2015-01-22 20:11:37 +0700

    delete unused file

--- resources/views/layouts/frontend/header.blade.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar text-center hidden-print" role="navigation">
-    <img style="height:50px;" src="{{ asset('images/logo-kejari.jpg') }}" alt=""/>
-    <h1>Monitoring Kasus</h1>
-    <h2>Kejaksaan Negeri Jember</h2>
-</nav>
