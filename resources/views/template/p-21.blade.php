c57773019c516ab1974693b1fc1091d44683ae42
uyab <uyab.exe@gmail.com>
2015-01-23 11:27:02 +0700

    indent paragraph

--- resources/views/template/p-21.blade.php
+++ resources/views/template/p-21.blade.php
@@ -24,7 +24,7 @@
 				<td width="5px">:</td>
 				<td width="300px">B- /0.5.12/Ep.1/I/2015</td>
 				<td width="40px" rowspan='4'></td>
-				<td>Jember, {{strftime("%e %B %Y")}}</td>				
+				<td>Jember, {{strftime("%e %B %Y")}}</td>
 			</tr>
 			<tr>
 				<td>Sifat</td>
@@ -33,12 +33,12 @@
 				<td rowspan='3'><p>
 			KEPADA YTH.
 		</p>
-		
+
 		<p>
 			KEPALA KEPOLISIAN SEKTOR KALIWATES<BR/>
-		
+
 			DI - <BR/>
-		
+
 			 <CENTER><u>…………………………</u></CENTER>
 		</p></td>
 			</tr>
@@ -59,8 +59,8 @@
 <p>
 	 <span style='width:50px'></span>Sehubungan dengan penyerahan berkas perkara pidana atas nama tersangka {{$case->suspectNames()}} Nomor BP/09/XII/2014/Reskrim tanggal {{strftime("%e %B %Y")}} yang kami terima tanggal {{strftime("%e %B %Y")}} setelah dilakukan penelitian ternyata hasil penyidikannya sudah lengkap.
 </p><br/>
-<p>
-	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sesuai dengan ketentuan pasal 8 ayat (3) b, pasal 138 ayat (1) dan pasal 139 KUHAP supaya Saudara menyerahkan tanggung jawab tersangka dan barang bukti kepada kami, guna menentukan apakah perkara tersebut sudah memenuhi persyaratan untuk dapat atau tidak dilimpahkan ke Pengadilan
+<p class="indent">
+Sesuai dengan ketentuan pasal 8 ayat (3) b, pasal 138 ayat (1) dan pasal 139 KUHAP supaya Saudara menyerahkan tanggung jawab tersangka dan barang bukti kepada kami, guna menentukan apakah perkara tersebut sudah memenuhi persyaratan untuk dapat atau tidak dilimpahkan ke Pengadilan
 </p></td>
 			</tr>
 			<tr>
@@ -71,13 +71,13 @@
 				</td>
 			</tr>
 
-		</table>		
-	
+		</table>
+
 <table>
 <tbody>
 <tr>
 	<td style='width:400px'>
-		
+
 	</td>
 	<td>
 		<br/><br/><br/><br/>
