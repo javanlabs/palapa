b011bed9d55ca6623639d0c7d5fde69eb902cb82
uyab <uyab.exe@gmail.com>
2015-02-03 14:50:22 +0700

    revert tampilan list kasus

--- resources/views/backend/cases/index.blade.php
+++ resources/views/backend/cases/index.blade.php
@@ -22,51 +22,14 @@
             <!-- /input-group -->
         </form>
 
-        <div class="list-case">
+        <div class="panel panel-default">
+            <div class="panel-heading">{{ count($cases) }} Perkara Perlu Penanganan</div>
+            <table class="table table-case">
                 @foreach($cases as $item)
-                    @include('modules.case.list', ['item' => $item, 'phases' => $item->phases()])
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h5 class="mb-0">{{ $item['name'] }}</h5>--}}
-                            {{--{{strtoupper($item->category)}} - <small class="text-muted">{{ $item['spdp_number'] }}</small>--}}
-                            {{--<br/>--}}
-                            {{--<strong>Tersangka</strong>--}}
-                            {{--@foreach($item->suspects as $suspect)--}}
-                                {{--{{$suspect->name}}--}}
-                            {{--@endforeach--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<div><i class="ion-person"></i> {{ $item['prosecutor_name'] }}</div>--}}
-                            {{--<hr style="margin-top: 10px; margin-bottom:10px; border-style: dashed none none"/>--}}
-                            {{--<div><i class="ion-ios-people"></i> {{ $item['penyidik_name'] }}</div>--}}
-
-                        {{--</td>--}}
-                        {{--<td width='100px'>--}}
-                            {{--<span class="badge badge-status {{ $item['status'] }}">{{ $item->getLatestActivityAttribute() }}</span>--}}
-                            {{--@if($item['is_suspend'])--}}
-                                {{--<div class="label label-danger label-lg">Perkara Dihentikan</div>--}}
-                            {{--@endif--}}
-                            {{--<span class="label label-default {{ $item['type_name'] }}">{{ $item['type_name'] }}</span>--}}
-                            {{--<small class="text-muted">--}}
-                            {{--<i>{{ $item->getLatestActivityAttribute()?$item->getLatestActivityAttribute()->name:'' }}</i>--}}
-                        {{--</small>--}}
-                        {{--</td>--}}
-                        {{--<td class="text-right">--}}
-                            {{--<div class="btn-group">--}}
-                                {{--@if(Auth::user()->canManage($item))--}}
-                                    {{--<a class="btn btn-default btn-sm" href="{{ $item['permalink_edit'] }}">--}}
-                                        {{--Edit <i class="ion-ios-arrow-forward"></i>--}}
-                                    {{--</a>--}}
-                                {{--@else--}}
-                                    {{--<a class="btn btn-default btn-sm btn-detail" href="{{ $item['permalink'] }}">Info</a>--}}
-                                {{--@endif--}}
-
-
-                            {{--</div>--}}
-                        {{--</td>--}}
-                    {{--</tr>--}}
+                    @include('modules.case.row', ['item' => $item, 'phases' => $item->phases()])
                 @endforeach
-            <div class="pad text-center">
+            </table>
+            <div class="panel-footer">
                 {{ $cases->appends('q', Input::get('q'))->render() }}
             </div>
         </div>
