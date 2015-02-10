187816f860a84934cf2f558d0bac3e87fcb2f84e
uyab <uyab.exe@gmail.com>
2015-02-10 11:29:09 +0700

    change db-backup scheduler interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('app:db-backup')->everyFiveMinutes();
+        $schedule->command('app:db-backup')->twiceDaily();
 	}
 
 }
