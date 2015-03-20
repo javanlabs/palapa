ec4cc26da1bda165dc323d6adbdf52cbeeb9f052
uyab <uyab.exe@gmail.com>
2015-03-20 13:31:40 +0700

    hilangkan semua menu kiri di datun

--- resources/views/frontend/tab.blade.php
+++ resources/views/frontend/tab.blade.php
@@ -1,18 +1,18 @@
 <div class="list-group list-group-menu">
+    @if($type != App\Cases\Cases::TYPE_DATUN)
     <a href="{{ route('frontend.search') }}?type={{ $type }}" class="list-group-item ellipsis {{ ($active == 'perkara')?'active':''}}">
         <span class="badge"><i class="fa-2x ion-ios-arrow-forward"></i></span>
         Daftar Perkara
     </a>
-    @if($type != App\Cases\Cases::TYPE_DATUN)
     <a href="{{ route('frontend.sidang') }}?type={{ $type }}" class="list-group-item ellipsis {{ ($active == 'sidang')?'active':''}}">
         <span class="badge"><i class="fa-2x ion-ios-arrow-forward"></i></span>
         Jadwal Sidang
     </a>
-    @endif
     <a href="{{ route('statistic.index') }}?type={{ $type }}" class="list-group-item ellipsis {{ ($active == 'statistik')?'active':''}}">
         <span class="badge"><i class="fa-2x ion-ios-arrow-forward"></i></span>
         Statistik
     </a>
+    @endif
 
     @foreach($allPostInCategory as $item)
         <a href="{{ route('frontend.post.byCaseType', ['id' => $item['id']]) }}" class="list-group-item ellipsis {{ ($active == $item['id'])?'active':''}}">
